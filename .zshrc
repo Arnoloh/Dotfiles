0d83df1355a2d08b750132bc2e746b845d988c0d
Arnoloh <arnaud.corcione@epita.fr>
2024-07-23 19:16:56 +0200

    Auto-Submission

--- .zshrc
+++ .zshrc
@@ -8,7 +8,6 @@ export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="mytheme"
 
-command -v macchina >/dev/null && macchina || echo "Macchina command do not exits, please install it"
 
 plugins=(
     copypath
@@ -19,23 +18,10 @@ plugins=(
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
-
-
-
-alias tree="eza -T "
+PATH= $PATH:/~/.config/command
 alias py="python3"
 alias vim="nvim"
-alias vi="nvim"
-alias ls="eza "
 alias cat="bat -pp"
 
 unsetopt autocd
 
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-export PATH="$HOME/bin:/Library/Frameworks/Python.framework/Versions/3.10/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public:/opt/X11/bin:~/.dotnet/tools:/Library/Apple/usr/bin:/Users/arnoloh/Library/Application Support/JetBrains/Toolbox/scripts:/Users/arnoloh/bin:/Users/arnoloh/.config/command:/Users/arnoloh/.docker/bin"
-source "$HOME/.cargo/env"
-
-alias make="make -j16"
-alias utmlctl=/Applications/UTM.app/Contents/MacOS/utmctl
-export PATH="/usr/local/opt/bison/bin:$PATH"
-export PATH="/opt/homebrew/opt/bison/bin:$PATH"
--- init.sh
+++ init.sh
@@ -9,22 +9,25 @@ echec() {
     echo "${RED}$1${WHITE}" 
 }
 
-# git clone  git@github.com:Arnoloh/Dotfiles.git /tmp/dotfiles
-# cd /tmp/dotfiles
-#
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
-#
-# cp /tmp/dotfiles/.zshrc ~/
-# cp /tmp/dotfiles/.mytheme.zsh-theme ~/.oh-my-zsh/themes/
+git clone  git@github.com:Arnoloh/Dotfiles.git /tmp/dotfiles
+cd /tmp/dotfiles
 
-# cp /tmp/dotfiles/.gitignore ~/
-# cp /tmp/dotfiles/.gitconfig ~/
-# cp /tmp/dotfiles/.clang-format ~/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+
+cp /tmp/dotfiles/.zshrc ~/
+cp /tmp/dotfiles/mytheme.zsh-theme ~/.oh-my-zsh/themes/
+
+cp /tmp/dotfiles/.gitignore ~/
+cp /tmp/dotfiles/.gitconfig ~/
+cp /tmp/dotfiles/.clang-format ~/
 
 [ -d ~/.config/ ] || mkdir ~/.config && pass ".config already exist"
 [ -d ~/.config/command ] || mkdir ~/.config/command && pass ".config/command already exist"
 
 
 cp /tmp/dotfiles/sub ~/.config/command/
-chmod x+u ~/.config/command/sub 
+chmod 777 ~/.config/command/sub 
 
+sed -i "s/macos/$(grep -E '^(NAME)=' /etc/os-release | sed 's/NAME="\(.*\)"/\1/g')/g" ~/.oh-my-zsh/themes/mytheme.zsh-theme
+
+echec "Please install manually all plugin"
